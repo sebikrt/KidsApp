a05e07e15f41cf6248e3acf706cb10a3cd80f9c6
sebikrt <sebi_krt@yahoo.com>
2018-11-26 23:33:11 +0200

    test from vs

--- KidsApp/KidsApp/ViewController.cs
+++ KidsApp/KidsApp/ViewController.cs
@@ -13,6 +13,7 @@ namespace KidsApp
         public override void ViewDidLoad ()
         {
             base.ViewDidLoad ();
+
             // Perform any additional setup after loading the view, typically from a nib.
         }
 
